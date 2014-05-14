93ba01fcfe9764b3e5f1745328b0e7a11e403a4c
DavidCWebs <david@carawebs.com>
2014-05-14 19:48:36 +0100

    Learndash mods

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '25f223bfa9c0ec1ca558ac4f6a0c658c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b5ce48ec727af60f3901f5b0b931575c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
