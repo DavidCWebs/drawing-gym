67d866d0a52edc3fd48f6ef2fe421bb08c8dbf7a
DavidCWebs <david@carawebs.com>
2014-05-28 15:42:12 +0100

    Page mods

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,30 +1,6 @@
 <?php while (have_posts()) : the_post(); ?>
+  <div class="col-md-8">
   <?php the_content(); ?>
-  <?php
-  // WP_Query arguments
-    $args = array (
-        'post_type'              => 'lessons',
-        'category_name'          => 'Gensler',
-        'posts_per_page'         => '10',
-        'posts_per_archive_page' => '10',
-        'order'                  => 'DESC',
-    );
-
-    // The Query
-    $lessonquery = new WP_Query( $args );
-
-    // The Loop
-    if ( $lessonquery->have_posts() ) {
-        while ( $lessonquery->have_posts() ) {
-            $lessonquery->the_post();
-            // do something
-        }
-    } else {
-        // no posts found
-    }
-
-    // Restore original Post Data
-    wp_reset_postdata();
-    ?>
+  </div>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
